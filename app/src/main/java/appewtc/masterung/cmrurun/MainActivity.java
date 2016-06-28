a57e7f7264167b9167ee53b4d35cc0d94edd011f
masterUNG <phrombutr@gmail.com>
2016-06-28 16:00:33 +0700

    Finish 28June

--- app/src/main/java/appewtc/masterung/cmrurun/MainActivity.java
+++ app/src/main/java/appewtc/masterung/cmrurun/MainActivity.java
@@ -1,17 +1,63 @@
 package appewtc.masterung.cmrurun;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private static final String urlLogo = "http://swiftcodingthai.com/cmru/cmru_logo.png";
+    private ImageView imageView;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+        //Load Logo
+        Picasso.with(this).load(urlLogo).resize(150,180).into(imageView);
+
     }   // Main Method
+
+    //Create Inner Class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return null;
+        }   // doInBack
+
+    }   // SynUser Class
+
+
+    public void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "Have Space", "Please Fill All Every Blank");
+        }
+
+
+    }   // clickSignIn
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
